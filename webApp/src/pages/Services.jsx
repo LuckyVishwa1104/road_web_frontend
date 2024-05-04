61f4078a383c965877681c7d1f601bc43b73c0df
Lucky Vishwakarma <110558983+LuckyVishwa1104@users.noreply.github.com>
2024-05-04 18:13:24 +0530

    added proper gird system

--- webApp/src/pages/Services.jsx
+++ webApp/src/pages/Services.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import Layout from "./../components/Layout/Layout";
 import img1 from "../images/hole1.jpg";
 import img2 from "../images/water1.jpg";
 import img3 from "../images/crack1.jpeg";
@@ -14,9 +13,9 @@ export const Services = () => {
     { src: img1, name: 'Potholes' },
     { src: img2, name: 'Water Logging' },
     { src: img3, name: 'Cracks' },
-    { src: img4, name: 'Potholes' },
-    { src: img5, name: 'Water Logging' },
-    { src: img6, name: 'Cracks' },
+    { src: img4, name: 'Potholes ' },
+    { src: img5, name: 'Water Logging ' },
+    { src: img6, name: 'Cracks ' },
   ];
 
   const handleMouseEnter = (name) => {
@@ -32,49 +31,26 @@ export const Services = () => {
       <div className="my-15 text-center">
         <h4 className="font-bold my-2 text-2xl md:text-3xl">Service Categorization</h4>
         
-        <div className="flex flex-col md:flex-row justify-center items-center mx-10 md:mx-20 lg:mx-40">
-          <div className="flex justify-center items-center flex-wrap">
-            {services.slice(0, 3).map((service, index) => (
-              <div 
-                key={index} 
-                className="flex flex-col items-center m-5 cursor-pointer relative"
-                onMouseEnter={() => handleMouseEnter(service.name)}
-                onMouseLeave={handleMouseLeave}
-              >
-                <img 
-                  src={service.src} 
-                  alt={service.name} 
-                  className="w-40 h-40 rounded-lg object-cover"
-                />
-                <div className={`text-lg font-bold mt-3 ${hoveredService === service.name ? 'visible opacity-100' : 'invisible opacity-0'} transition-opacity duration-300 absolute bottom-10 left-1/2 transform -translate-x-1/2 bg-white bg-opacity-75 p-2 rounded`}>{service.name}</div>
-              </div>
-            ))}
-          </div>
-          
-          <div className="flex justify-center items-center flex-wrap mt-10 md:mt-0">
-            {services.slice(3, 6).map((service, index) => (
-              <div 
-                key={index} 
-                className="flex flex-col items-center m-5 cursor-pointer relative"
-                onMouseEnter={() => handleMouseEnter(service.name)}
-                onMouseLeave={handleMouseLeave}
-              >
-                <img 
-                  src={service.src} 
-                  alt={service.name} 
-                  className="w-40 h-40 rounded-lg object-cover"
-                />
-                <div className={`text-lg font-bold mt-3 ${hoveredService === service.name ? 'visible opacity-100' : 'invisible opacity-0'} transition-opacity duration-300 absolute bottom-10 left-1/2 transform -translate-x-1/2 bg-white bg-opacity-75 p-2 rounded`}>{service.name}</div>
-              </div>
-            ))}
-          </div>
+        <div className="grid grid-cols-3 gap-10 mx-10 md:mx-20 lg:mx-80">
+          {services.map((service, index) => (
+            <div 
+              key={index} 
+              className="flex flex-col items-center m-5 cursor-pointer relative"
+              onMouseEnter={() => handleMouseEnter(service.name)}
+              onMouseLeave={handleMouseLeave}
+            >
+              <img 
+                src={service.src} 
+                alt={service.name} 
+                className="w-40 h-40 rounded-lg object-cover"
+              />
+              <div className={`text-lg font-bold mt-3 ${hoveredService === service.name ? 'visible opacity-100' : 'invisible opacity-0'} transition-opacity duration-300 absolute bottom-10 left-1/2 transform -translate-x-1/2 bg-white bg-opacity-75 p-2 rounded`}>{service.name}</div>
+            </div>
+          ))}
         </div>
-        
         
         {/* Display hovered service name */}
       </div>
     </>
   );
 };
-
-// export default Services;
